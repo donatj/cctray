5f8993a2a12b496dc403e650a62dbc9737d883bc
Jesse Donat <donatj@gmail.com>
2016-06-27 14:24:15 -0500

    Adds docs

--- ccproject.go
+++ ccproject.go
@@ -2,29 +2,39 @@ package ccproject
 
 import "encoding/xml"
 
+// Activity is the type for the Activity response attribute
+type Activity string
+
+const (
+	// ActSleeping indicates activity state is "Sleeping"
+	ActSleeping Activity = "Sleeping"
+	// ActBuilding indicates activity state is "Building"
+	ActBuilding Activity = "Building"
+	// ActCheckingModifications indicates activity state is "CheckingModifications"
+	ActCheckingModifications Activity = "CheckingModifications"
+)
+
+// Status is the type for the LastBuildStatus response attribute
+type Status string
+
+const (
+	// StatusSuccess indicates last build state was successful
+	StatusSuccess Status = "Success"
+	// StatusFailure indicates last build state was an expected failure
+	StatusFailure Status = "Failure"
+	// StatusException indicates the last build was an unexpected failure
+	StatusException Status = "Exception"
+	// StatusUnknown indicates last build state is not known
+	StatusUnknown Status = "Unknown"
+)
+
+// CCProjects represents the base XML <Projects> node which contains projects
 type CCProjects struct {
 	XMLName  xml.Name     `xml:"Projects"`
 	Projects []*CCProject `xml:"Project"`
 }
 
-type Activity string
-
-const (
-	ActSleeping Activity = "Sleeping"
-	ActBuilding Activity = "Building"
-
-	ActCheckingModifications Activity = "CheckingModifications"
-)
-
-type Status string
-
-const (
-	StatusSuccess   Status = "Success"
-	StatusFailure   Status = "Failure"
-	StatusException Status = "Exception"
-	StatusUnknown   Status = "Unknown"
-)
-
+// CCProject represents a <Project> node
 type CCProject struct {
 	XMLName         xml.Name `xml:"Project"`
 	Name            string   `xml:"name,attr"`
